c2e857bde0c8f4e82f2a6c135437c83be25fca60
lazizcodes <labdullaev02@mail.ru>
2021-05-16 13:12:38 +0500

    Working version

--- TicTacToe.js
+++ TicTacToe.js
@@ -5,20 +5,37 @@ for (let i = 0; i < 9; i++) {
     state.push(0);
 }
 
-// for(let i = 0; i < input.length; i++) {
-//     console.log(state[i]);
-// }
-
 const winningStates = [
-    [0,1,2], 
-    [3,4,5], 
-    [6,7,8], 
-    [0,3,6], 
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
 ]
+
+function isSubset(arr1, arr2) {
+    return arr2.every(e => arr1.includes(e))
+}
+
+function checkState() {
+    const ones = [];
+    const zeros = [];
+
+    state.forEach((e, i) => {
+        if (e === 1) ones.push(i);
+        if (e === 2) zeros.push(i);
+    })
+
+    for (let s of winningStates) {
+        if (isSubset(ones, s) || isSubset(zeros, s)) {
+            alert('Game Over!')
+            return;
+        }
+    }
+}
 
 let lastAct = 'O';
 
@@ -34,12 +51,14 @@ input.forEach((e, i) => {
         } else if (lastAct === 'X') {
             lastAct = 'O';
             state[i] = 2;
-            e.textContent = 'O'
+            e.textContent = 'O';
         }
+        checkState();
     });
 })
 
 let button = document.querySelector("#restartBttn")
 button.addEventListener('click', function () {
     input.forEach(e => e.textContent = "")
+    lastAct = 'O';
 })
